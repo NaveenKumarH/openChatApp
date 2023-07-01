c3ef9bcc5c23a8b8c5f785821a87b21d05f51637
Naveen Kumar H <hgng1234@gmail.com>
2023-07-01 11:54:06 +0530

    Added get user by id method

--- Back-End/src/apis/userApi.js
+++ Back-End/src/apis/userApi.js
@@ -1,1 +1,11 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const userApi = express.Router();
+const User = require("../models/User");
+userApi.get("/user/:id", async (req, res) => {
+  try {
+    const data = await User.findById(req.params.id);
+    res.send(data).status(200);
+  } catch (err) {
+    res.send(err.message).status(400);
+  }
+});
