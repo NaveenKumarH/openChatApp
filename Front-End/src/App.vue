6c9c5f098b9db691acc6660134cb00137fb95b34
Naveen Kumar H <hgng1234@gmail.com>
2023-07-06 13:41:00 +0530

    Added the opener component in FE

---
+++ Front-End/src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Opener from './components/Opener.vue'
+</script>
+
+<template>
+  <Opener />
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+
+.div_main {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  padding-left: 75px;
+  padding-right: 75px;
+}
+
+.div_boxContent {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  height: 50%;
+  background-color: aliceblue;
+  border-radius: 20px;
+  margin-left: 10px;
+  padding: 50px;
+  margin-right: 10px;
+  font-size: 5rem;
+  justify-content: center;
+  align-items: center;
+}
+
+.textContent {
+  background-color: aliceblue;
+}
+input{
+    margin: 10px;
+    font-size: 2rem;
+    border-radius: 5px;
+}
+@media screen and (max-width: 940px) {
+  .div_boxContent {
+    font-size: 3rem;
+  }
+  input{
+    font-size: 1.5rem;
+  }
+}
+@media screen and (max-width: 640px) {
+  .div_boxContent {
+    font-size: 2rem;
+  }
+  input{
+    font-size: 1rem;
+  }
+}
+
+</style>
---
+++ Front-End/src/components/Opener.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <div class="div_main">
+        <div class="div_boxContent">
+            <div class="textContent">Your username!!</div>
+            <input type="text" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
