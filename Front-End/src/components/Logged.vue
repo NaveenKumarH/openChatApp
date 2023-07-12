aca9ce4229f974bafefa15e1e1fb2288da57e424
Naveen Kumar H <hgng1234@gmail.com>
2023-07-12 20:13:41 +0530

    Added Logged component

---
+++ Front-End/src/components/Logged.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({ user: String })
+</script>
+<template>
+    <div class="div_main">
+        <div class="div_boxContent">
+            <h5>Hola!! {{ props.user }}</h5>
+        </div>
+
+    </div>
+</template>
+<style scoped>
+
+</style>
