3d9100e33e3e74ec5d0da51b60b9776213f34557
Naveen Kumar H <hgng1234@gmail.com>
2023-07-12 22:51:34 +0530

    Added post and delete to UserApi

--- Back-End/src/apis/userApi.js
+++ Back-End/src/apis/userApi.js
@@ -9,4 +9,20 @@ userApi.get("/user/:id", async (req, res) => {
     res.send(err.message).status(400);
   }
 });
+userApi.post("/user", async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.send(req.body).status(201);
+  } catch (err) {
+    res.send(err.message).status(400);
+  }
+});
+userApi.delete("/user/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send(data).status(200);
+  } catch (err) {
+    res.send(err.message).status(400);
+  }
+});
 module.exports = userApi;
